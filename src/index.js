19770820ce11debcb2a79cdf2ffe1cf2d516f215
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-05-23 12:30:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/react-bc50-1-budget-boss">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/react-bc50-1-budget-boss">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>
 );
