2e8fc3146a4d3833d203b97636548ed7e3bf5080
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-05-24 21:57:45 +0300

    костыли номер 2

--- src/modules/moduleReports/components/GoHomeLink/GoHomeLink.jsx
+++ src/modules/moduleReports/components/GoHomeLink/GoHomeLink.jsx
@@ -1,5 +1,20 @@
+// const GoHomeLink = () => {
+//     return <span>стрілочка Main page</span>
+// };
+
+// export default GoHomeLink;
+
+import { useLocation } from 'react-router';
+import f from './GoHomeLink.module.css';
+
 const GoHomeLink = () => {
-    return <span>стрілочка Main page</span>
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? '/';
+  return (
+    <div to={backLinkHref} className={f.goBackLink}>
+      <span>GO BACK</span>
+    </div>
+  );
 };
 
 export default GoHomeLink;
