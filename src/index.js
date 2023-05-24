15a23c1878f7b7ec6053a8f609eec24f9c1992b3
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-05-24 10:33:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
