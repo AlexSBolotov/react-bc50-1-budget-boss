35baff625bbcfcef6931f7fb68bbf3e8456bd644
Helen <51207721+ElenaKorenieva@users.noreply.github.com>
2023-05-25 10:47:35 +0200

    Transactions mobile component added (#36)
    * added general styles, downloaded and imported fomts, downloaded and squashed images
    
    * changed colors in body styles
    
    * added summary component
    
    * deleted unused branch, pull all changes from main branch
    
    * add sceleton for mobile transaction list component

--- src/modules/moduleHome/components/TransactionContainer/TransactionContainer.jsx
+++ src/modules/moduleHome/components/TransactionContainer/TransactionContainer.jsx
@@ -1,11 +1,13 @@
 import TransactionsSummary from 'modules/moduleHome/components/TransactionsSummary/TransactionsSummary';
 import TransactionsList from 'modules/moduleHome/components/TransactionsList/TransactionsList';
+import TransactionsListMobile from '../TransactionListMobile/TransactionListMobile';
 
 const TransactionContainer = () => {
   return (
     <section>
       <TransactionsList />
       <TransactionsSummary />
+      <TransactionsListMobile />
     </section>
   );
 };
