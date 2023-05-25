2dbf937f54ca86130d092e170a6eef9b9156398a
Volodymyr <68303955+Preinspiron@users.noreply.github.com>
2023-05-25 10:27:53 +0200

    fone (#35)

--- src/modules/moduleLayout/components/Layout/Layout.jsx
+++ src/modules/moduleLayout/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import Header from 'modules/moduleLayout/components/Header/Header';
 import Loader from 'modules/shared/components/Loader/Loader';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import s from './Layout.module.css';
+import s from 'modules/moduleLayout/components/Layout/Layout.module.scss';
 
 const Layout = () => {
   return (
