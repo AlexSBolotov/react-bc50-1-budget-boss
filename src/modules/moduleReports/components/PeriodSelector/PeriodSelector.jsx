2bbee69d02b20bc26e8f0d3975fa8cfd58435d40
V_Borysenko <preinspiron@gmail.com>
2023-05-25 23:11:42 +0200

    done

--- src/modules/moduleReports/components/PeriodSelector/PeriodSelector.jsx
+++ src/modules/moduleReports/components/PeriodSelector/PeriodSelector.jsx
@@ -1,22 +1,32 @@
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 import s from 'modules/moduleReports/components/PeriodSelector/PeriodSelector.module.scss';
 import date from 'date-and-time';
-import { useState, useEffect } from 'react';
+import { useState, useCallback } from 'react';
+
+// const test = Date.now();
+// console.log(test);
+// console.log(date.parse('2015/01/02 23:14:05', 'YYYY/MM/DD'));
+// console.log(test);
 
 const PeriodSelector = ({ onClick }) => {
-  const [data, setData] = useState(0);
-  const now = new Date();
-  let nextMonth = date.addMonths(now, data);
+  const data = new Date();
+
+  // const [data, setData] = useState(new Date());
+  const [count, setCount] = useState(0);
+
+  const newData = date.addMonths(data, count);
+
+  const fnData = useCallback(val => date.addMonths(newData, val), [newData]);
+
   const handleData = val => {
-    setData(prev => prev + val);
-    nextMonth = date.addMonths(now, val);
-    console.log(val, nextMonth);
-    onClick(date.format(nextMonth, 'YYYY-MM'));
+    setCount(prev => prev + val);
+
+    onClick(date.format(fnData(val), 'YYYY-MM'));
   };
 
-  useEffect(() => {
-    onClick(date.format(now, 'YYYY-MM'));
-  }, []);
+  // useEffect(() => {
+  //   onClick(date.format(now, 'YYYY-MM'));
+  // }, []);
 
   return (
     <div className={s.dataBlock}>
@@ -29,7 +39,7 @@ const PeriodSelector = ({ onClick }) => {
             onClick={() => handleData(-1)}
           />
         </button>
-        <span>{date.format(nextMonth, 'MMMM YYYY')}</span>
+        <span>{date.format(newData, 'MMMM YYYY')}</span>
 
         <button className={s.btn} onClick={() => handleData(1)}>
           <MdKeyboardArrowRight size="20" color="green" />
