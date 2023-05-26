acecaa31f7d73892db4728f869425ac0711e8cd2
Volodymyr <68303955+Preinspiron@users.noreply.github.com>
2023-05-26 19:06:59 +0200

    done (#56)
    * done
    
    * done

--- src/modules/moduleReports/components/ReportGraph/ReportGraph.jsx
+++ src/modules/moduleReports/components/ReportGraph/ReportGraph.jsx
@@ -5,9 +5,11 @@ import s from './ReportGraph.module.css';
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 
-export default function ReportGraph() {
+export default function ReportGraph({ data }) {
   const isMobile = useMediaQuery({ maxWidth: 768 });
   console.log(isMobile);
+  console.log(data);
+
   return (
     <div className={s.bar}>
       {isMobile ? <ReportGraphHorizontal /> : <ReportGraphVertical />}
