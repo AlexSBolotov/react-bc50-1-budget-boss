7cab839dab995b8f952ce1eb936767c43b136137
Olena Korenieva <olena.korenieva@gmail.com>
2023-05-26 09:10:15 +0200

    translated russian titles of months to English, added styles to mobile transaction list

--- src/modules/moduleHome/components/TransactionDate/TransactionDate.jsx
+++ src/modules/moduleHome/components/TransactionDate/TransactionDate.jsx
@@ -1,40 +1,38 @@
-import { useState,forwardRef } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import { useState, forwardRef } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import './TransactionDate.scss';
 
 import { enGB } from 'date-fns/locale';
 
 const TransactionDate = () => {
-    const [startDate, setStartDate] = useState(new Date());
-    const ExampleCustomInput = forwardRef(({ value, onClick, onChange }, ref) => (
-        <input
-        value={value}
-        className="icon-input"
-        onClick={onClick}
-        onChange={onChange}
-        ref={ref}
-        onKeyDown={(e) => {
-            e.preventDefault();
-        }}
-      ></input>
-      ));
-    return (
-      <div className="wrapper-container">
+  const [startDate, setStartDate] = useState(new Date());
+  const ExampleCustomInput = forwardRef(({ value, onClick, onChange }, ref) => (
+    <input
+      value={value}
+      className="icon-input"
+      onClick={onClick}
+      onChange={onChange}
+      ref={ref}
+      onKeyDown={e => {
+        e.preventDefault();
+      }}
+    ></input>
+  ));
+
+  console.log(startDate);
+  return (
+    <div className="wrapper-container">
       <DatePicker
         dateFormat="dd.MM.yyyy"
         selected={startDate}
-        onChange={(date) => setStartDate(date)}
+        onChange={date => setStartDate(date)}
         locale={enGB}
         maxDate={new Date()}
-        customInput={<ExampleCustomInput/>}
-        
-        
-        
-        
+        customInput={<ExampleCustomInput />}
       />
-      </div>
-    );
-  };
-  export default TransactionDate;
-// остается логика и позиционирование вместе с формой 
+    </div>
+  );
+};
+export default TransactionDate;
+// остается логика и позиционирование вместе с формой
