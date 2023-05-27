c9df6e39867650e41d90d84b4849f9c757d0da09
Helen <51207721+ElenaKorenieva@users.noreply.github.com>
2023-05-27 18:41:26 +0200

    Fix components home (#76)
    * added general styles, downloaded and imported fomts, downloaded and squashed images
    
    * changed colors in body styles
    
    * added summary component
    
    * deleted unused branch, pull all changes from main branch
    
    * add sceleton for mobile transaction list component
    
    * translated russian titles of months to English, added styles to mobile transaction list
    
    * add logic to TransactionSummary and TransactionListMobileComponents
    
    * fixed logic of TransactionSummary component
    
    * fixed logic of TransactionSummaryComponent
    
    * fixed TransactionSummary

--- src/modules/moduleHome/components/TransactionDate/TransactionDate.jsx
+++ src/modules/moduleHome/components/TransactionDate/TransactionDate.jsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { useState, forwardRef } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './TransactionDate.scss';
--- src/modules/moduleHome/components/TransactionsSummary/TransactionsSummary.jsx
+++ src/modules/moduleHome/components/TransactionsSummary/TransactionsSummary.jsx
@@ -1,4 +1,4 @@
-import s from './TransactionsSummary.module.css';
+import s from './TransactionsSummary.module.scss';
 import { useSelector } from 'react-redux';
 import { monthTranslation } from './monthTranslation';
 import {
@@ -45,36 +45,56 @@ const TransactionsSummary = () => {
   }
 
   return (
-    <>
-      (
-      <div className={s.summaryWrapper}>
-        <table className={s.summaryTable}>
-          <thead className={s.summaryHead}>
-            <tr>
-              <th colSpan="2" className={s.summaryTableHeading}>
-                Summary
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {summaryData.map(el => {
-              if (el[1] === 'N/A') {
-                el[1] = 0;
-              }
-              return (
-                <tr key={`${el[0]}${el[1]}`}>
-                  <td className={s.summaryTableCell}>
-                    {monthTranslation(el[0])}
-                  </td>
-                  <td className={s.summaryTableCell}>{el[1]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      )
-    </>
+    <div className={s.summaryWrapper}>
+      <table className={s.summaryTable}>
+        <thead className={s.summaryHead}>
+          <tr>
+            <th colSpan="2" className={s.summaryTableHeading}>
+              Summary
+            </th>
+          </tr>
+        </thead>
+        <tbody className={s.summaryBody}>
+          {summaryData.map(el => {
+            if (el[1] === 'N/A') {
+              el[1] = 0;
+            }
+            return (
+              <tr key={`${el[0]}${el[1]}`} className={s.summaryLine}>
+                <td className={s.summaryTableCell}>
+                  {monthTranslation(el[0])}
+                </td>
+                <td className={s.summaryTableCell}>{el[1]}</td>
+              </tr>
+            );
+          })}
+          <tr style={{ height: 38 }} className={s.summaryLine}>
+            <td className={s.summaryTableCell}></td>
+            <td className={s.summaryTableCell}></td>
+          </tr>
+          <tr style={{ height: 38 }} className={s.summaryLine}>
+            <td className={s.summaryTableCell}></td>
+            <td className={s.summaryTableCell}></td>
+          </tr>
+          <tr style={{ height: 38 }} className={s.summaryLine}>
+            <td className={s.summaryTableCell}></td>
+            <td className={s.summaryTableCell}></td>
+          </tr>
+          <tr style={{ height: 38 }} className={s.summaryLine}>
+            <td className={s.summaryTableCell}></td>
+            <td className={s.summaryTableCell}></td>
+          </tr>
+          <tr style={{ height: 38 }} className={s.summaryLine}>
+            <td className={s.summaryTableCell}></td>
+            <td className={s.summaryTableCell}></td>
+          </tr>
+          <tr style={{ height: 38 }} className={s.summaryLine}>
+            <td className={s.summaryTableCell}></td>
+            <td className={s.summaryTableCell}></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
