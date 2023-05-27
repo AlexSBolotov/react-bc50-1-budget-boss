9a05c8047ab4eb40870e634bb98007a0364d0874
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-05-27 13:16:58 +0300

    balance(style)
    не могу до конца доложить стили по странице home так как нет общей картинки нижней части страницы.

--- src/modules/moduleReports/components/GoHomeLink/GoHomeLink.jsx
+++ src/modules/moduleReports/components/GoHomeLink/GoHomeLink.jsx
@@ -1,12 +1,14 @@
 import { useLocation } from 'react-router';
 import f from './GoHomeLink.module.css';
+import svgBack from '../../../shared/images/svg/backLeftArrow.svg'
 
 const GoHomeLink = () => {
   const location = useLocation();
   const backLinkHref = location.state?.from ?? '/';
   return (
     <div to={backLinkHref} className={f.goBackLink}>
-      <span> {'<--'} Main Page</span>
+        <img src={svgBack} alt="Back" className={f.back} />
+        Main Page
     </div>
   );
 };
--- src/modules/shared/components/Balance/Balance.jsx
+++ src/modules/shared/components/Balance/Balance.jsx
@@ -1,29 +1,3 @@
-// import s from './Balance.module.css';
-
-// const Balance = () => {
-
-//   return (
-//     <>
-//       <h2 className={s.balance_title}>Balance:</h2>
-//       <input
-//         className={s.balance_value}
-//         type="number"
-//         name="balance"
-//         title="Please, enter your balance"
-//         pattern="[0-9, .UAH]*"
-//         placeholder={`00.00 UAH`}
-//         required
-//       />
-//       <button type="submit" className={s.balance_submit}>
-//         Confirm
-//       </button>
-//     </>
-//   );
-
-// }
-
-// export default Balance;
-
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
