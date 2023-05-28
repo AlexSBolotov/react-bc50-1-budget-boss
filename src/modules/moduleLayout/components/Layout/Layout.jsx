65f4709e8fd7e7e9e0daef7613268754b5ed4e6e
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-05-28 19:25:52 +0300

    shared_container_001

--- src/modules/moduleLayout/components/Layout/Layout.jsx
+++ src/modules/moduleLayout/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Loader } from 'modules/shared/components/Loader/Loader';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-// import s from 'modules/moduleLayout/components/Layout/Layout.module.scss';
+import s from 'modules/moduleLayout/components/Layout/Layout.module.scss';
 import { useSelector } from 'react-redux';
 import { selectIsLoading } from 'redux/auth/authSelectors';
 import Container from 'modules/shared/components/Container/Container';
@@ -10,12 +10,14 @@ const Layout = () => {
   const isLoading = useSelector(selectIsLoading);
   return (
     <>
-      <Container>
-        {isLoading && <Loader />}
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
-      </Container>
+      <main className={s.container}>
+        <Container>
+          {isLoading && <Loader />}
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+        </Container>
+      </main>
     </>
   );
 };
