e432ab16be8f04a16d9de09e9b70779bd394025c
V_Borysenko <preinspiron@gmail.com>
2023-05-28 12:33:23 +0200

    123

--- src/modules/moduleReports/components/ReportGraph/ReportGraph.jsx
+++ src/modules/moduleReports/components/ReportGraph/ReportGraph.jsx
@@ -6,7 +6,6 @@ import s from './ReportGraph.module.css';
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 import { selectAllExpenses, selectAllIncomes } from 'redux/store';
-import { MdLocalHospital } from 'react-icons/md';
 
 export default function ReportGraph({ data, flag }) {
   const isMobile = useMediaQuery({ maxWidth: 480 });
