10734c8a856268821a543780ed07ee2d6d7f6de5
Helen <51207721+ElenaKorenieva@users.noreply.github.com>
2023-05-28 16:18:17 +0200

    Fixed styles for mobile transactions component (#91)
    * added general styles, downloaded and imported fomts, downloaded and squashed images
    
    * changed colors in body styles
    
    * added summary component
    
    * deleted unused branch, pull all changes from main branch
    
    * add sceleton for mobile transaction list component
    
    * translated russian titles of months to English, added styles to mobile transaction list
    
    * add logic to TransactionSummary and TransactionListMobileComponents
    
    * fixed logic of TransactionSummary component
    
    * fixed logic of TransactionSummaryComponent
    
    * fixed TransactionSummary
    
    * fixed styles
    
    * fixed styles for mobile list with transactions

--- src/modules/moduleHome/components/TransactionContainer/TransactionContainer.jsx
+++ src/modules/moduleHome/components/TransactionContainer/TransactionContainer.jsx
@@ -1,21 +1,24 @@
 import { useMediaQuery } from 'react-responsive';
-import s from './TransactionContainer.module.css';
+import s from './TransactionContainer.module.scss';
 
 import TransactionsSummary from 'modules/moduleHome/components/TransactionsSummary/TransactionsSummary';
 import TransactionsList from 'modules/moduleHome/components/TransactionsList/TransactionsList';
 import TransactionsListMobile from '../TransactionListMobile/TransactionListMobile';
 
-
 const TransactionContainer = ({ selectedDate }) => {
-   const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   //  const isTablet = useMediaQuery({ maxWidth: 1199 });
   //  const isDesktop = useMediaQuery({ maxWidth: 1279 });
   return (
-    <section className={s.container}>
-      {isMobile ? <TransactionsListMobile selectedDate={selectedDate} /> :<div className={s.tab_deskContainer}><TransactionsList selectedDate={selectedDate} />
-      <TransactionsSummary /></div> }
-      
-      
+    <section className={s.sectionContainer}>
+      {isMobile ? (
+        <TransactionsListMobile selectedDate={selectedDate} />
+      ) : (
+        <div className={s.tab_deskContainer}>
+          <TransactionsList selectedDate={selectedDate} />
+          <TransactionsSummary />
+        </div>
+      )}
     </section>
   );
 };
--- src/modules/moduleLayout/components/Layout/Layout.jsx
+++ src/modules/moduleLayout/components/Layout/Layout.jsx
@@ -1,20 +1,21 @@
 import { Loader } from 'modules/shared/components/Loader/Loader';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import s from 'modules/moduleLayout/components/Layout/Layout.module.scss';
+// import s from 'modules/moduleLayout/components/Layout/Layout.module.scss';
 import { useSelector } from 'react-redux';
 import { selectIsLoading } from 'redux/auth/authSelectors';
+import Container from 'modules/shared/components/Container/Container';
 
 const Layout = () => {
   const isLoading = useSelector(selectIsLoading);
   return (
     <>
-      <div className={s.container}>
+      <Container>
         {isLoading && <Loader />}
         <Suspense fallback={<Loader />}>
           <Outlet />
         </Suspense>
-      </div>
+      </Container>
     </>
   );
 };
