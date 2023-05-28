9d1c78183c073349400529bc140497a188271cba
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-05-28 20:45:36 +0300

    main_container_styles

--- src/modules/moduleLayout/components/Layout/Layout.jsx
+++ src/modules/moduleLayout/components/Layout/Layout.jsx
@@ -1,23 +1,24 @@
 import { Loader } from 'modules/shared/components/Loader/Loader';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import s from 'modules/moduleLayout/components/Layout/Layout.module.scss';
+// import s from 'modules/moduleLayout/components/Layout/Layout.module.scss';
 import { useSelector } from 'react-redux';
 import { selectIsLoading } from 'redux/auth/authSelectors';
 import Container from 'modules/shared/components/Container/Container';
+import MainContainer from '../MainContainer/MainContainer';
 
 const Layout = () => {
   const isLoading = useSelector(selectIsLoading);
   return (
     <>
-      <main className={s.container}>
+      <MainContainer>
         <Container>
           {isLoading && <Loader />}
           <Suspense fallback={<Loader />}>
             <Outlet />
           </Suspense>
         </Container>
-      </main>
+      </MainContainer>
     </>
   );
 };
